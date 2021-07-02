a676ee4b484b1dfddf850bc09638a909973a42d8
jodyu <jody_unterschutz@tcenergy.com>
2021-07-02 11:19:56 -0600

    Working on bootstrap and stuff

--- JodyCore2.Web/jody-game/src/components/TeamComponents.tsx
+++ JodyCore2.Web/jody-game/src/components/TeamComponents.tsx
@@ -9,31 +9,25 @@ interface Props {
 
 export const TeamList: React.FC<ListProps> = ({ teams }) => {
   return (
-    <ul>
-      {teams.map((object, i) => <TeamListItem team={object} key={i} />)}
-    </ul>
+    <div className="container">      
+          {teams.map((object, i) => <TeamListItem team={object} key={i} />)}
+    </div>    
   );
 }
 
 export const TeamListItem: React.FC<Props> = ({ team }) => {
-  return (
-  <li>
-    <label>{ team.name }</label><label>{ team.skill }</label>
-  </li>
+  return (  
+    <div className="row">
+      <div className="col-sm">
+        { team.name }    
+      </div>    
+      <div className="col-sm">
+        { team.skill }
+      </div>
+      <div className="col-sm">
+        { team.identifier }
+      </div>
+    </div>
+  
   );
 };
-
-async function getTeams(): Promise<Team[]> {
-
-  // For now, consider the data is stored on a static `users.json` file
-  return fetch('https://localhost:5000/api/Team/all')
-          // the JSON body is taken from the response
-          .then(res => res.json())
-          .then(res => {
-                  // The response has an `any` type, so we need to cast
-                  // it to the `User` type, and return it from the promise
-                  return res as Team[]
-          })
-}
-
-export default getTeams
