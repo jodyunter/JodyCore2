8215752baf7f20e4cb6d13d1df7333c41d52626b
jodyu <jody_unterschutz@tcenergy.com>
2021-08-20 13:07:00 -0600

    Need to work on viewmodels for standings items

---
+++ JodyCore2.Service/Mappers/StandingsMapper.cs
@@ -0,0 +1,17 @@
+﻿using JodyCore2.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JodyCore2.Service.Mappers
+{
+    public class StandingsMapper
+    {
+        public IStandingsViewModel StandingsToStandingsViewModel(IStandings standings)
+        {
+
+        }
+    }
+}
---
+++ JodyCore2.Service/Mappers/StandingsRecordMapper.cs
@@ -0,0 +1,19 @@
+﻿using JodyCore2.Domain;
+using JodyCore2.Domain.Bo.Standings;
+using JodyCore2.Service.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JodyCore2.Service.Mappers
+{
+    public class StandingsRecordMapper
+    {
+        public IStandingsRecordViewModel StandingsRecordToStandingsRecordViewModel(IStandingsRecord record)
+        {
+            return null;
+        }
+    }
+}
