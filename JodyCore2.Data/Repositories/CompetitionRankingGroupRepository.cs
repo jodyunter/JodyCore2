da670c8035c082124594312cfcff749202467129
jodyu <jody_unterschutz@tcenergy.com>
2021-09-10 13:14:19 -0600

    Something with the games not being created properly now that we moved to competition instead of standings

--- JodyCore2.Data/Repositories/CompetitionRankingGroupRepository.cs
+++ JodyCore2.Data/Repositories/CompetitionRankingGroupRepository.cs
@@ -1,4 +1,5 @@
 ﻿using JodyCore2.Domain.Bo.Competition;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,12 @@ namespace JodyCore2.Data.Repositories
     {
         public IQueryable<CompetitionRankingGroup> GetByCompetition(Guid competitionIdentifier, JodyContext context)
         {
-            return context.CompetitionRankingGroups.Where(t => t.Identifier == competitionIdentifier);
+            return AlwaysInclude(context.CompetitionRankingGroups.Where(t => t.Competition.Identifier == competitionIdentifier));
+        }
+
+        public override IQueryable<CompetitionRankingGroup> AlwaysInclude(IQueryable<CompetitionRankingGroup> query)
+        {
+            return query.Include(g => g.Rankings).ThenInclude(r => r.Team);
         }
     }
 }
