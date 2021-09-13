6eaf0fc6a05f758db57d100adbeba2b94b2f79f7
jodyu <jody_unterschutz@tcenergy.com>
2021-09-13 10:01:09 -0600

    next

--- JodyCore2.Test.Xunit.Data/Repositories/Competitions/TestCompetitionGameRepository.cs
+++ JodyCore2.Test.Xunit.Data/Repositories/Competitions/TestCompetitionGameRepository.cs
@@ -1,5 +1,8 @@
 ﻿using JodyCore2.Data;
 using JodyCore2.Data.Repositories;
+using JodyCore2.Data.Repositories.Competitions;
+using JodyCore2.Data.Repositories.Teams;
+using JodyCore2.Domain.Bo;
 using JodyCore2.Domain.Bo.Competitions;
 using System;
 using System.Collections.Generic;
@@ -11,29 +14,158 @@ namespace JodyCore2.Test.Xunit.Data.Repositories.Competitions
 {
     public class TestCompetitionGameRepository : TestBaseRepository<CompetitionGame>
     {
+        ICompetitionGameRepository gameRepository;
+        ITeamRepository teamRepository;
+
         public override CompetitionGame SetupCreateData(JodyContext context)
         {
-            throw new NotImplementedException();
-        }
+            var 
+            var home = new Team(Guid.NewGuid(), "Team 1", 5);
+            var away = new Team(Guid.NewGuid(), "Team 2", 5);
 
-        public override IList<CompetitionGame> SetupDeleteData(JodyContext context)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IList<CompetitionGame> SetupGetAllData(JodyContext context)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IBaseRepository<CompetitionGame> SetupRepository()
-        {
-            throw new NotImplementedException();
+            return new CompetitionGame(Guid.NewGuid(), 25, 5, home, away, 25, 36, true, false, true);
         }
 
         public override CompetitionGame SetupUpdateData(CompetitionGame originalData, JodyContext context)
         {
-            throw new NotImplementedException();
+            var team3 = new Team(Guid.NewGuid(), "Team 3", 5);
+            var team4 = new Team(Guid.NewGuid(), "team 4", 5);
+
+            context.Add(team3);
+            context.Add(team4);
+
+            context.SaveChanges();
+
+            var updatedData = gameRepository.GetByIdentifier(originalData.Identifier, context).FirstOrDefault();
+
+            updatedData.Home = team3;
+            updatedData.Away = team4;
+
+            return updatedData;
         }
+
+        public override IList<CompetitionGame> SetupDeleteData(JodyContext context)
+        {
+            return SetupGetAllData(context);
+        }
+        public override IList<CompetitionGame> SetupGetAllData(JodyContext context)
+        {
+            var teams = TestTeamRepository.SetupGenericTeams(20, context, teamRepository);
+            var list = new List<CompetitionGame>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var gameDto = new CompetitionGame(Guid.NewGuid(), 15, 1, teams[i], teams[i + 10], 0, 0, false, false, true);
+                gameRepository.Create(gameDto, context);
+
+                list.Add(gameDto);
+            }
+
+            return list;
+        }
+
+        public override IBaseRepository<CompetitionGame> SetupRepository()
+        {
+            teamRepository = new TeamRepository();
+            gameRepository = new CompetitionGameRepository();
+            return gameRepository;
+        }
+
+
+        protected void SetupCompetitionGameData(JodyContext context)
+        {
+            var gameDtos = new List<CompetitionGame>()
+            {
+                new CompetitionGame(Guid.NewGuid(), 1, 1, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 2, 1, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 2, 1, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 3, 1, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 3, 1, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 3, 1, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 1, 2, null, null, 0, 0, true, false, true),
+                new CompetitionGame(Guid.NewGuid(), 1, 2, null, null, 0, 0, true, false, true),
+                new CompetitionGame(Guid.NewGuid(), 1, 2, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 1, 2, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 1, 2, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 1, 2, null, null, 0, 0, false, false, true),
+                new CompetitionGame(Guid.NewGuid(), 2, 2, null, null, 0, 0, true, false, true),
+                new CompetitionGame(Guid.NewGuid(), 2, 2, null, null, 0, 0, true, false, true),
+                new CompetitionGame(Guid.NewGuid(), 2, 2, null, null, 0, 0, true, false, true),
+                new CompetitionGame(Guid.NewGuid(), 2, 2, null, null, 0, 0, false, false, true),
+            };
+
+            gameRepository.Create(gameDtos, context);
+        }
+
+        [Fact]
+        public void ShouldGetByYearAndDateRangeNullLastDay()
+        {
+            using (var context = new JodyContext())
+            {
+                SetupCompetitionGameData(context);
+                context.SaveChanges();
+            }
+
+            using (var context = new JodyContext())
+            {
+                Assert.StrictEqual(6, gameRepository.GetByYearAndDayRange(1, 1, null, context).Count());
+                Assert.StrictEqual(10, gameRepository.GetByYearAndDayRange(2, 1, null, context).Count());
+            }
+        }
+
+        [Fact]
+        public void ShouldGetByYearAndDateRangeNotNullLastDay()
+        {
+            using (var context = new JodyContext())
+            {
+                SetupCompetitionGameData(context);
+                context.SaveChanges();
+            }
+
+            using (var context = new JodyContext())
+            {
+                Assert.StrictEqual(1, gameRepository.GetByYearAndDayRange(1, 1, 1, context).Count());
+                Assert.StrictEqual(3, gameRepository.GetByYearAndDayRange(1, 1, 2, context).Count());
+                Assert.StrictEqual(6, gameRepository.GetByYearAndDayRange(1, 1, 3, context).Count());
+                Assert.StrictEqual(6, gameRepository.GetByYearAndDayRange(2, 1, 1, context).Count());
+                Assert.StrictEqual(10, gameRepository.GetByYearAndDayRange(2, 1, 2, context).Count());
+                Assert.StrictEqual(4, gameRepository.GetByYearAndDayRange(2, 2, 2, context).Count());
+            }
+        }
+
+        [Fact]
+        public void GetByYearAndDayRangeAndCompleteStatusTrue()
+        {
+            using (var context = new JodyContext())
+            {
+                SetupCompetitionGameData(context);
+                context.SaveChanges();
+            }
+
+            using (var context = new JodyContext())
+            {
+                Assert.StrictEqual(0, gameRepository.GetByYearAndDayRangeAndCompleteStatus(1, 1, 5, true, context).Count());
+                Assert.StrictEqual(3, gameRepository.GetByYearAndDayRangeAndCompleteStatus(2, 2, 5, true, context).Count());
+            }
+        }
+
+        //todo: need to figure out how to verify the data returned per game is correct
+        [Fact]
+        public void GetByYearAndDayRangeAndCompleteStatusFalse()
+        {
+            using (var context = new JodyContext())
+            {
+                SetupCompetitionGameData(context);
+                context.SaveChanges();
+            }
+
+            using (var context = new JodyContext())
+            {
+                Assert.StrictEqual(6, gameRepository.GetByYearAndDayRangeAndCompleteStatus(1, 1, 5, false, context).Count());
+                Assert.StrictEqual(1, gameRepository.GetByYearAndDayRangeAndCompleteStatus(2, 2, 2, false, context).Count());
+            }
+        }
+
     }
+}
 }
--- JodyCore2.Test.Xunit.Data/Utility.cs
+++ JodyCore2.Test.Xunit.Data/Utility.cs
@@ -1,4 +1,5 @@
-﻿using JodyCore2.Domain.Bo.Standings;
+﻿using JodyCore2.Domain.Bo.Competitions;
+using JodyCore2.Domain.Bo.Standings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,27 @@ namespace JodyCore2.Test.Xunit.Data
         public static IStandings CreateStandingsNoRecords()
         {
             return new Standings(Guid.NewGuid(), "Standings Name", 1, 1, 1, 250, "Test", "Test this", new List<IStandingsRecord>());
+        }
+    }
+
+    public class TestCompetition : Competition
+    {
+        public TestCompetition(CompetitionType type):base(type) { CompetitionType = type; }
+
+        public TestCompetition(Guid identifier, string name, int startYear, int endYear, int startDay, int endDay, string description, CompetitionType type) : this(type)
+        {
+            Identifier = identifier;
+            Name = name;
+            StartYear = startYear;
+            EndYear = endYear;
+            StartDay = startDay;
+            EndDay = endDay;
+            Description = description;
+        }
+
+        public override void ProcessGame(ICompetitionGame game)
+        {
+            throw new NotImplementedException();
         }
     }
 }
